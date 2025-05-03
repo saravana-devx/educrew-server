b287b81894d1ec4b90ff42d4c6696b3af0ecc7d5
venom8261 <saravana8261@gmail.com>
2025-05-03 16:46:59 +0530

    forth commit

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,9 +1,9 @@
-import User from "../../src/model/user";
-
+// import User from "../../src/model/user";
+import { IUser } from "../../src/interfaces/interface";
 declare global {
   namespace Express {
     interface Request {
-      currentUser: User;
+      currentUser: IUser;
     }
   }
 }
